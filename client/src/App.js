dd73ce4147f3d016570d9349539d0720bc16469d
jroyals2 <jamesroyals3@gmail.com>
2017-12-21 23:56:44 -0500

    created some basic HTML and toggle function for loggedin few vs logged out view

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components'
 
 class App extends Component {
+
+  /* 
+  I want to created a toggle for 2 separate home pages. one for logged in and one for not.
+  I will use this state in order to do this
+  */
+
+  state = {
+    users: ['Player 1', 'James'],
+    loggedIn: false
+  }
+
+  toggleLoggedIn = () => {
+    this.setState({loggedIn : !this.state.loggedIn})
+  }
   render() {
+    // this will be the state of the website when you are logged out
+    const isLoggedOut = <div>
+      <h3>Please select a user to get started. If you don't see your user then create a new one!</h3>
+      <ul>
+        {this.state.users.map((user) => {
+          return <li>{user}</li>
+        })}
+      </ul>
+      <button onClick={this.toggleLoggedIn}>Create A New User!</button>
+    </div>
+
+    const isLoggedIn = <div>
+      <h3>Select a Game or Press New Game to Get Started!</h3>
+      <button onClick={this.toggleLoggedIn}>New Game</button>
+
+    </div>
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Welcome to Connect Four!</h1>
+        {this.state.loggedIn ? isLoggedIn : isLoggedOut}
       </div>
     );
   }
