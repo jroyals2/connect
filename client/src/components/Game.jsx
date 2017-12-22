50eff02d5f0b3cdc53157c56e7a42494391cba4b
jroyals2 <jamesroyals3@gmail.com>
2017-12-22 16:44:25 -0500

    added player moves logic for player1 and player 2

--- client/src/components/Game.jsx
+++ client/src/components/Game.jsx
@@ -8,10 +8,33 @@ class Game extends Component {
                     [0,0,0,0,0,0,0],
                     [0,0,0,0,0,0,0],
                     [0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0]]
+                    [0,0,0,0,0,0,0]],
+        player1: true,
+        player2: false
+
     }
 
-    
+    playerMove = (index) => {
+        if (this.state.player1 === true) {
+          for (let i = this.state.gameBoard.length - 1; i >= 0; i--) {
+            if (this.state.gameBoard[i][index] === 0) {
+              this.state.gameBoard[i][index] = 1;
+              this.state.player2 = true;
+              this.state.player1 = false;
+              break;
+            }
+          }
+        } else if (this.state.player2 === true) {
+          for (let i = this.state.gameBoard.length - 1; i >= 0; i--) {
+            if (this.state.gameBoard[i][index] === 0) {
+              this.state.gameBoard[i][index] = 2;
+              this.state.player2 = false;
+              this.state.player1 = true;
+              break;
+            }
+          }
+        }
+      };
     render() {
         return (
             <div>
