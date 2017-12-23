68ca5a9d613a5242483fe097572362a54c77cc56
jroyals2 <jamesroyals3@gmail.com>
2017-12-22 19:14:29 -0500

    added some functioning buttons that place 1 or 2 depending on player's turn

--- client/src/components/Game.jsx
+++ client/src/components/Game.jsx
@@ -19,8 +19,8 @@ class Game extends Component {
           for (let i = this.state.gameBoard.length - 1; i >= 0; i--) {
             if (this.state.gameBoard[i][index] === 0) {
               this.state.gameBoard[i][index] = 1;
-              this.state.player2 = true;
-              this.state.player1 = false;
+              this.setState({player2: true})
+              this.setState({player1: false})
               break;
             }
           }
@@ -28,17 +28,27 @@ class Game extends Component {
           for (let i = this.state.gameBoard.length - 1; i >= 0; i--) {
             if (this.state.gameBoard[i][index] === 0) {
               this.state.gameBoard[i][index] = 2;
-              this.state.player2 = false;
-              this.state.player1 = true;
+              this.setState({player1: true})
+              this.setState({player2: false})
               break;
             }
           }
         }
       };
+
     render() {
         return (
             <div>
-                {this.state.gameBoard}
+                {this.state.gameBoard.map((array, index) => {
+                    return <div index={index}>{array}</div>
+                })}
+                <button onClick={()=>this.playerMove(0)}>Column1</button>
+                <button onClick={()=>this.playerMove(1)}>Column2</button>
+                <button onClick={()=>this.playerMove(2)}>Column3</button>
+                <button onClick={()=>this.playerMove(3)}>Column4</button>
+                <button onClick={()=>this.playerMove(4)}>Column5</button>
+                <button onClick={()=>this.playerMove(5)}>Column6</button>
+                <button onClick={()=>this.playerMove(6)}>Column7</button>
             </div>
         );
     }
