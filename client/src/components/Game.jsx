0a7182532bd07605e992d8bb0aff8c6e1e3e9b30
jroyals2 <jamesroyals3@gmail.com>
2017-12-25 11:17:51 -0500

    added win logic for diagonal down and up

--- client/src/components/Game.jsx
+++ client/src/components/Game.jsx
@@ -21,21 +21,40 @@ class Game extends Component {
                     console.log('player 1 wins column')
                 } else if (this.state.gameBoard[i][j] === 2 && this.state.gameBoard[i-1][j] === 2 && this.state.gameBoard[i-2][j] === 2 && this.state.gameBoard[i-3][j] === 2){
                     console.log('player 2 wins column')
-                } else {
-                    console.log('keep it up')
-                }
+                } 
             }
         }
     }
     winConditionRow = () => {
         for (let i = this.state.gameBoard.length - 1; i >= 0; i--) {
-            for (let j = 0; j < 3; j++)
-            if (this.state.gameBoard[i][j] === 1 && this.state.gameBoard[i][j+1] === 1 && this.state.gameBoard[i][j+2] === 1 && this.state.gameBoard[i][j+3] === 1) {
-                console.log("player 1 wins rows")
-            } else if (this.state.gameBoard[i][j] === 2 && this.state.gameBoard[i][j+1] === 2 && this.state.gameBoard[i][j+2] === 2 && this.state.gameBoard[i][j+3] === 2) {
-                console.log("player 2 wins rows")
-             } else  {
-                console.log('The game is afoot')
+            for (let j = 0; j < 3; j++) {
+                if (this.state.gameBoard[i][j] === 1 && this.state.gameBoard[i][j + 1] === 1 && this.state.gameBoard[i][j + 2] === 1 && this.state.gameBoard[i][j + 3] === 1) {
+                    console.log("player 1 wins rows")
+                } else if (this.state.gameBoard[i][j] === 2 && this.state.gameBoard[i][j + 1] === 2 && this.state.gameBoard[i][j + 2] === 2 && this.state.gameBoard[i][j + 3] === 2) {
+                    console.log("player 2 wins rows")
+                }
+            }
+        }
+    }
+    winConditionDiagUp = () => {
+        for (let i = this.state.gameBoard.length - 1; i > 3; i--) {
+            for (let j = 0; j < 3; j++) {
+                if (this.state.gameBoard[i][j] === 1 && this.state.gameBoard[i - 1][j + 1] === 1 && this.state.gameBoard[i - 2][j + 2] === 1 && this.state.gameBoard[i - 3][j + 3] === 1) {
+                    console.log('Player 1 wins Diag Up')
+                } else if (this.state.gameBoard[i][j] === 2 && this.state.gameBoard[i - 1][j + 1] === 2 && this.state.gameBoard[i - 2][j + 2] === 2 && this.state.gameBoard[i - 3][j + 3] === 2) {
+                    console.log('Player 2 wins Diag Up')
+                }
+            }
+        }
+    }
+    winConditionDiagDown = () => {
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (this.state.gameBoard[i][j] === 1 && this.state.gameBoard[i + 1][j + 1] === 1 && this.state.gameBoard[i + 2][j + 2] === 1 && this.state.gameBoard[i + 3][j + 3] === 1) {
+                    console.log('Player 1 wins Diag Down')
+                } else if (this.state.gameBoard[i][j] === 2 && this.state.gameBoard[i + 1][j + 1] === 2 && this.state.gameBoard[i + 2][j + 2] === 2 && this.state.gameBoard[i + 3][j + 3] === 2) {
+                    console.log('Player 2 wins Diag Down')
+                }
             }
         }
     }
@@ -49,6 +68,8 @@ class Game extends Component {
                     this.setState({gameBoard: newGameBoard})
                     this.winConditionRow()
                     this.winConditionColumn()
+                    this.winConditionDiagUp()
+                    this.winConditionDiagDown()
                     this.setState({ player2: true })
                     this.setState({ player1: false })
                     break;
@@ -62,6 +83,8 @@ class Game extends Component {
                     this.setState({gameBoard: newGameBoard})
                     this.winConditionRow()
                     this.winConditionColumn()
+                    this.winConditionDiagUp()
+                    this.winConditionDiagDown()
                     this.setState({ player1: true })
                     this.setState({ player2: false })
                     break;
