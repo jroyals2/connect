5fc6f5e78eb7bbc32943ec31a271677f0c7d27ca
jroyals2 <jamesroyals3@gmail.com>
2017-12-26 14:36:51 -0500

    added User and Game models

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import Landing from './components/Landing.jsx'
 import Game from './components/Game.jsx'
+import OnePlayerGame from './components/OnePlayerGame.jsx'
 
 class App extends Component {
 
@@ -14,6 +15,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={Landing} />
           <Route exact path="/game" component={Game}/>
+          <Route exact path="/oneplayergame" component={OnePlayerGame}/>
         </Switch>
       </div>
       </Router>
---
+++ client/src/components/OnePlayerGame.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class OnePlayerGame extends Component {
+    render() {
+        return (
+            <div>
+                Hello from One Player Game
+            </div>
+        );
+    }
+}
+
+export default OnePlayerGame;
