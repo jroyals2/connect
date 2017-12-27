55ce32b61f80f57d53660065f186da650a3b1976
jroyals2 <jamesroyals3@gmail.com>
2017-12-26 19:44:50 -0500

    added comments throughout the game component

--- client/src/components/Game.jsx
+++ client/src/components/Game.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const GameGrid = styled.div`
 display: table;         
@@ -51,31 +51,36 @@ class Game extends Component {
         filledOut: false
 
     }
+
+    // Handles the changes for the form
+
     toggleFilledOut = (event) => {
         event.preventDefault()
-        this.setState({filledOut: !this.state.filledOut})
+        this.setState({ filledOut: !this.state.filledOut })
     }
     handleChangeOne = (event) => {
         const playerOneName = event.target.value
-        this.setState({playerOneName})
+        this.setState({ playerOneName })
     }
     handleChangeTwo = (event) => {
         const playerTwoName = event.target.value
-        this.setState({playerTwoName})
+        this.setState({ playerTwoName })
     }
 
+
+    // These are all the win conditions
     winConditionColumn = () => {
-        for(let i = this.state.gameBoard.length - 1; i > 3; i--) {
-            for (let j = 0; j < 6; j++){
-                if (this.state.gameBoard[i][j] === 'one' && this.state.gameBoard[i-1][j] === 'one' && this.state.gameBoard[i-2][j] === 'one' && this.state.gameBoard[i-3][j] === 'one'){
+        for (let i = this.state.gameBoard.length - 1; i > 3; i--) {
+            for (let j = 0; j < 6; j++) {
+                if (this.state.gameBoard[i][j] === 'one' && this.state.gameBoard[i - 1][j] === 'one' && this.state.gameBoard[i - 2][j] === 'one' && this.state.gameBoard[i - 3][j] === 'one') {
                     console.log('player 1 wins column')
-                    this.setState({playerOneWin: true})
-                    this.setState({killGame: true})
-                } else if (this.state.gameBoard[i][j] === 'two' && this.state.gameBoard[i-1][j] === 'two' && this.state.gameBoard[i-2][j] === 'two' && this.state.gameBoard[i-3][j] === 'two'){
+                    this.setState({ playerOneWin: true })
+                    this.setState({ killGame: true })
+                } else if (this.state.gameBoard[i][j] === 'two' && this.state.gameBoard[i - 1][j] === 'two' && this.state.gameBoard[i - 2][j] === 'two' && this.state.gameBoard[i - 3][j] === 'two') {
                     console.log('player 2 wins column')
-                    this.setState({playerTwoWin: true})
-                    this.setState({killGame: true})
-                } 
+                    this.setState({ playerTwoWin: true })
+                    this.setState({ killGame: true })
+                }
             }
         }
     }
@@ -84,12 +89,12 @@ class Game extends Component {
             for (let j = 0; j < 4; j++) {
                 if (this.state.gameBoard[i][j] === 'one' && this.state.gameBoard[i][j + 1] === 'one' && this.state.gameBoard[i][j + 2] === 'one' && this.state.gameBoard[i][j + 3] === 'one') {
                     console.log("player 1 wins rows")
-                    this.setState({playerOneWin: true})
-                    this.setState({killGame: true})
+                    this.setState({ playerOneWin: true })
+                    this.setState({ killGame: true })
                 } else if (this.state.gameBoard[i][j] === 'two' && this.state.gameBoard[i][j + 1] === 'two' && this.state.gameBoard[i][j + 2] === 'two' && this.state.gameBoard[i][j + 3] === 'two') {
                     console.log("player 2 wins rows")
-                    this.setState({playerTwoWin: true})
-                    this.setState({killGame: true})
+                    this.setState({ playerTwoWin: true })
+                    this.setState({ killGame: true })
                 }
             }
         }
@@ -99,12 +104,12 @@ class Game extends Component {
             for (let j = 0; j < 4; j++) {
                 if (this.state.gameBoard[i][j] === 'one' && this.state.gameBoard[i - 1][j + 1] === 'one' && this.state.gameBoard[i - 2][j + 2] === 'one' && this.state.gameBoard[i - 3][j + 3] === 'one') {
                     console.log('Player 1 wins Diag Up')
-                    this.setState({playerOneWin: true})
-                    this.setState({killGame: true})
+                    this.setState({ playerOneWin: true })
+                    this.setState({ killGame: true })
                 } else if (this.state.gameBoard[i][j] === 'two' && this.state.gameBoard[i - 1][j + 1] === 'two' && this.state.gameBoard[i - 2][j + 2] === 'two' && this.state.gameBoard[i - 3][j + 3] === 'two') {
                     console.log('Player 2 wins Diag Up')
-                    this.setState({playerTwoWin: true})
-                    this.setState({killGame: true})
+                    this.setState({ playerTwoWin: true })
+                    this.setState({ killGame: true })
                 }
             }
         }
@@ -114,17 +119,18 @@ class Game extends Component {
             for (let j = 0; j < 4; j++) {
                 if (this.state.gameBoard[i][j] === 'one' && this.state.gameBoard[i + 1][j + 1] === 'one' && this.state.gameBoard[i + 2][j + 2] === 'one' && this.state.gameBoard[i + 3][j + 3] === 'one') {
                     console.log('Player 1 wins Diag Down')
-                    this.setState({playerOneWin: true})
-                    this.setState({killGame: true})
+                    this.setState({ playerOneWin: true })
+                    this.setState({ killGame: true })
                 } else if (this.state.gameBoard[i][j] === 'two' && this.state.gameBoard[i + 1][j + 1] === 'two' && this.state.gameBoard[i + 2][j + 2] === 'two' && this.state.gameBoard[i + 3][j + 3] === 'two') {
                     console.log('Player 2 wins Diag Down')
-                    this.setState({playerTwoWin: true})
-                    this.setState({killGame: true})
+                    this.setState({ playerTwoWin: true })
+                    this.setState({ killGame: true })
                 }
             }
         }
     }
 
+    // This is the Player move logic for both players
 
     playerMove = (index) => {
         if (this.state.player1 === true) {
@@ -132,7 +138,7 @@ class Game extends Component {
                 if (this.state.gameBoard[i][index] === 0) {
                     let newGameBoard = this.state.gameBoard
                     newGameBoard[i][index] = 'one';
-                    this.setState({gameBoard: newGameBoard})
+                    this.setState({ gameBoard: newGameBoard })
                     this.winConditionRow()
                     this.winConditionColumn()
                     this.winConditionDiagUp()
@@ -147,7 +153,7 @@ class Game extends Component {
                 if (this.state.gameBoard[i][index] === 0) {
                     let newGameBoard = this.state.gameBoard
                     newGameBoard[i][index] = 'two';
-                    this.setState({gameBoard: newGameBoard})
+                    this.setState({ gameBoard: newGameBoard })
                     this.winConditionRow()
                     this.winConditionColumn()
                     this.winConditionDiagUp()
@@ -160,6 +166,8 @@ class Game extends Component {
         }
     };
 
+    // This resets the board to and state to the original 
+
     boardReset = () => {
         this.setState({
             gameBoard: [[0, 0, 0, 0, 0, 0, 0],
@@ -171,44 +179,46 @@ class Game extends Component {
         })
         this.setState({ player1: true })
         this.setState({ player2: false })
-        this.setState({playerOneWin: false})
-        this.setState({playerTwoWin: false})
-        this.setState({killGame: false})
-        this.setState({filledOut: false})
+        this.setState({ playerOneWin: false })
+        this.setState({ playerTwoWin: false })
+        this.setState({ killGame: false })
+        this.setState({ filledOut: false })
     }
- 
+
 
     render() {
-    const gameSetup =  <Container>
-    <GameGrid>
-    {this.state.gameBoard.map((array, index) => {
-        return <GameRows
-         key={index}>{array.map((each, index)=>{
-             return <GameColumns key={index} className={each}>{each}</GameColumns>
-         })}</GameRows>
-    })}
-    </GameGrid>
-    <div>
-        {this.state.killGame ? "Game Over! Press New Game to start over!" :
+        const gameSetup = <Container>
+            <GameGrid>
+                {this.state.gameBoard.map((array, index) => {
+                    return <GameRows
+                        key={index}>{array.map((each, index) => {
+                            return <GameColumns key={index} className={each}>{each}</GameColumns>
+                        })}</GameRows>
+                })}
+            </GameGrid>
             <div>
-                <button onClick={() => this.playerMove(0)}>Column1</button>
-                <button onClick={() => this.playerMove(1)}>Column2</button>
-                <button onClick={() => this.playerMove(2)}>Column3</button>
-                <button onClick={() => this.playerMove(3)}>Column4</button>
-                <button onClick={() => this.playerMove(4)}>Column5</button>
-                <button onClick={() => this.playerMove(5)}>Column6</button>
-                <button onClick={() => this.playerMove(6)}>Column7</button>
-            </div>}
-    </div>
-    <div><button onClick={this.boardReset}>New Game</button>
-        {this.state.player1 ? <div>{this.state.playerOneName}'s turn (Black Token) </div> : ''}
-        {this.state.player2 ? <div>{this.state.playerTwoName}'s turn (Red Token) </div> : ''}
-        {this.state.playerOneWin ? <div>{this.state.playerOneName} wins!</div> : ''}
-        {this.state.playerTwoWin ? <div>{this.state.playerTwoName} wins!</div> : ''}
-        <Link to="/">Home</Link>
-    </div>
-</Container>
-        const setup = <div>
+                {this.state.killGame ? "Game Over! Press New Game to start over!" :
+                    <div>
+                        <button onClick={() => this.playerMove(0)}>Column1</button>
+                        <button onClick={() => this.playerMove(1)}>Column2</button>
+                        <button onClick={() => this.playerMove(2)}>Column3</button>
+                        <button onClick={() => this.playerMove(3)}>Column4</button>
+                        <button onClick={() => this.playerMove(4)}>Column5</button>
+                        <button onClick={() => this.playerMove(5)}>Column6</button>
+                        <button onClick={() => this.playerMove(6)}>Column7</button>
+                    </div>}
+            </div>
+            <div><button onClick={this.boardReset}>New Game</button>
+                {this.state.player1 ? <div>{this.state.playerOneName}'s turn (Black Token) </div> : ''}
+                {this.state.player2 ? <div>{this.state.playerTwoName}'s turn (Red Token) </div> : ''}
+                {this.state.playerOneWin ? <div>{this.state.playerOneName} wins!</div> : ''}
+                {this.state.playerTwoWin ? <div>{this.state.playerTwoName} wins!</div> : ''}
+                <Link to="/">Home</Link>
+            </div>
+        </Container>
+        
+        const setup = 
+        <div>
             <form>
                 <div>
                     <label htmlFor="playerOneName">Player One Name: </label>
@@ -225,7 +235,7 @@ class Game extends Component {
 
         return (
             <div>
-            { this.state.filledOut ? gameSetup : setup }
+                {this.state.filledOut ? gameSetup : setup}
             </div>
         );
     }
