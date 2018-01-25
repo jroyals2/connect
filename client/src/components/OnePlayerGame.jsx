9200966ff24bca742fe889139553fbb2c393e46d
jroyals2 <jamesroyals3@gmail.com>
2018-01-25 14:14:26 -0500

    added some AI functionality

--- client/src/components/OnePlayerGame.jsx
+++ client/src/components/OnePlayerGame.jsx
@@ -109,35 +109,76 @@ class OnePlayerGame extends Component {
     }
 
     // AI for the computer to block you from a win
-    // computerCheckForBlockDiaDown = () => {
-    //     for (let i = 0; i < 4; i++) {
-    //         for (let j = 0; j < 5; j++) {
-    //             if (this.state.gameBoard[i][j] === 'one' && this.state.gameBoard[i + 1][j + 1] === 'one' && this.state.gameBoard[i + 2][j + 2] === 'one') {
-    //                 console.log('Player 1 is close to victory diag down')
-    // }
+    computerCheckForBlockDiaDown = () => {
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 5; j++) {
+                if (this.state.gameBoard[i][j] === 'one' && this.state.gameBoard[i + 1][j + 1] === 'one' && this.state.gameBoard[i + 2][j + 2] === 'one') {
+                    console.log('Player 1 is close to victory diag down')
+                }
+            }
+        }
+    }
+
+    computerCheckForBlockDiaUp = () => {
+        for (let i = this.state.gameBoard.length - 1; i > 4; i--) {
+            for (let j = 0; j < 5; j++) {
+                if (this.state.gameBoard[i][j] === 'one' && this.state.gameBoard[i - 1][j + 1] === 'one' && this.state.gameBoard[i - 2][j + 2] === 'one') {
+                    console.log('Player 1 is close to victory Diag Up')
+                }
+            }
+        }
+    }
+    computerCheckForBlockColumn = () => {
+        for (let i = this.state.gameBoard.length - 1; i > 3; i--) {
+            for (let j = 0; j < 7; j++) {
+                if (this.state.gameBoard[i][j] === 'one' && this.state.gameBoard[i - 1][j] === 'one' && this.state.gameBoard[i - 2][j] === 'one') {
+                    console.log('player 1 close to win column')
+                    let newGameBoard = this.state.gameBoard;
+                    newGameBoard[0][0] = 'two'
+                    this.setState({ gameBoard: newGameBoard })
+                    this.setState({ player1: true })
+                    this.setState({ computerTurn: false })
+                    break;
+                } else {
+                    console.log("keep going")
+                }
+            }
+        }
+    }
+    computerCheckForBlockRow = () => {
+        for (let i = this.state.gameBoard.length - 1; i >= 0; i--) {
+            for (let j = 0; j < 5; j++) {
+                if (this.state.gameBoard[i][j] === 'one' && this.state.gameBoard[i][j + 1] === 'one' && this.state.gameBoard[i][j + 2] === 'one') {
+                    console.log("player 1 close to win rows")
+
+                }
+            }
+        }
+    }
 
 
 
 
-
-
-    computerMove = () => { 
+    computerMove = () => {
+        this.computerCheckForBlockColumn()
         let index = Math.floor(Math.random() * 6)
-             for (let i = this.state.gameBoard.length - 1; i >= 0; i--) {
-                 if (this.state.gameBoard[i][index] === 0) {
-                     let newGameBoard = this.state.gameBoard
-                     newGameBoard[i][index] = 'two'
-                     this.setState({ gameBoard: newGameBoard })
-                     this.winConditionRow()
-                     this.winConditionColumn()
-                     this.winConditionDiagUp()
-                     this.winConditionDiagDown()
-                     this.setState({ player1: true })
-                     this.setState({ computerTurn: false })
-                     break;
-                 }
-           }
-     }
+        {
+            for (let i = this.state.gameBoard.length - 1; i >= 0; i--) {
+                if (this.state.gameBoard[i][index] === 0) {
+                    let newGameBoard = this.state.gameBoard
+                    newGameBoard[i][index] = 'two'
+                    this.setState({ gameBoard: newGameBoard })
+                    this.winConditionRow()
+                    this.winConditionColumn()
+                    this.winConditionDiagUp()
+                    this.winConditionDiagDown()
+                    this.setState({ player1: true })
+                    this.setState({ computerTurn: false })
+                    break;
+                }
+            }
+        }
+    }
     playerMove = (index) => {
         if (this.state.player1 === true) {
             for (let i = this.state.gameBoard.length - 1; i >= 0; i--) {
@@ -155,23 +196,23 @@ class OnePlayerGame extends Component {
                     break;
                 }
             }
-        }  
-    }; 
+        }
+    };
 
 
 
     render() {
 
         const gameSetup = <Container>
-        <GameGrid>
-            {this.state.gameBoard.map((array, index) => {
-                return <GameRows
-                    key={index}>{array.map((each, index) => {
-                        return <GameColumns key={index} className={each}>{each}</GameColumns>
-                    })}</GameRows>
-            })}
-        </GameGrid>
-        <div>
+            <GameGrid>
+                {this.state.gameBoard.map((array, index) => {
+                    return <GameRows
+                        key={index}>{array.map((each, index) => {
+                            return <GameColumns key={index} className={each}>{each}</GameColumns>
+                        })}</GameRows>
+                })}
+            </GameGrid>
+            <div>
                 {this.state.killGame ? "Game Over! Press New Game to start over!" :
                     <div>
                         <button onClick={() => this.playerMove(0)}>Column1</button>
